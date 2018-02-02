d3a309dea5af7f1ff72aa9d06b005c24b62a63ae
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 04:44:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,9 @@
 # Write your code here.
 def badge_maker(array)
   new_array = []
+  array.each_with_index do |item, index|
+    new_array[index] = "Hello, my name is #{item}"
+  end
+    
+  end
 end
