8763b3d269a8aeea634d01fa01541cf610db4bb6
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 04:48:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,11 @@ end
 def batch_badge_creator(array)
   new_array = badge_maker(array)
 end
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index do |item, index|
+    new_array[index] = "Hello, #{item}! You'll be assigned to room ##{index + 1}!"
+  end
+  new_array
+end
